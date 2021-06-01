aea55295ffe7dc82cb60d71f8bd4f6300aa52968
Dennison Bertram <dennison@dennisonbertram.com>
2021-06-01 16:17:12 -0400

    👌 IMPROVE: Add 4byte directory for Function Signatures

--- web/src/components/RefundableTargetList/RefundableTargetList.js
+++ web/src/components/RefundableTargetList/RefundableTargetList.js
@@ -1,21 +1,36 @@
-import { Text, Flex, useMediaQuery } from '@chakra-ui/react'
-import { shortenAddress } from '@usedapp/core'
+import { Text, Flex, Link, IconButton } from '@chakra-ui/react'
+import { SearchIcon } from '@chakra-ui/icons'
 
 const RefundableTargetList = ({ refunderRefundables }) => {
-  const [isLargerThan800] = useMediaQuery('(min-width: 800px)')
-
   return (
     <Flex justifyContent="space-between">
       <Flex flexDirection="column">
-        <Text>Target Contract Address</Text>
+        <Text marginBottom="1em">Contract Address</Text>
         {refunderRefundables.map((refundable, index) => (
           <Text key={index}>{refundable.target}</Text>
         ))}
       </Flex>
       <Flex flexDirection="column">
-        <Text>Function Signature</Text>
+        <Text  marginBottom="1em">Function Signature</Text>
         {refunderRefundables.map((refundable, index) => (
-          <Text key={index}>{refundable.identifier}</Text>
+          <Flex key={index} flexDirection="row">
+            <Link
+              href={
+                'https://www.4byte.directory/api/v1/signatures/?hex_signature=' +
+                refundable.identifier
+              }
+              isExternal
+            >
+              <Text>
+                {refundable.identifier}{' '}
+                <IconButton
+                  size="xs"
+                  aria-label="Search database"
+                  icon={<SearchIcon />}
+                />
+              </Text>{' '}
+            </Link>
+          </Flex>
         ))}
       </Flex>
     </Flex>
