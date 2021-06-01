80eaf581a1b0cade8cf29ed1982aef986b265eca
Dennison Bertram <dennison@dennisonbertram.com>
2021-06-01 14:27:25 -0400

    👌 IMPROVE: Get Deployed Refunder contract address

--- web/src/components/CreateRefunder/CreateRefunder.js
+++ web/src/components/CreateRefunder/CreateRefunder.js
@@ -4,12 +4,14 @@ import { useEthers, useContractFunction } from '@usedapp/core'
 import { useRefunderAddress } from '../../common/hooks/useRefunderAddress'
 import Factory from '../../common/ABI/RefunderFactory.json'
 import { useEffect, useState } from 'react'
+import { navigate, routes } from '@redwoodjs/router'
 
 const CreateRefunder = () => {
   const { library, chainId } = useEthers()
 
   const contractAddress = useRefunderAddress(chainId)
   const [contract, setContract] = useState(null)
+  const [createdRefunder, setCreatedRefunder] = useState(null)
 
   useEffect(() => {
     if (contractAddress) {
@@ -21,10 +23,49 @@ const CreateRefunder = () => {
 
   const { state, send } = useContractFunction(contract, 'createRefunder')
 
+  useEffect(() => {
+    const getConfirmedEvents = async () => {
+      // from: https://www.chainshot.com/article/ethers-cheat-sheet
+      const topic = contract.interface.getEventTopic('RefunderCreated')
+
+      const log = state.receipt.logs.find((x) => x.topics.indexOf(topic) >= 0)
+
+      // finally, you can parse the log with the escrow interface
+      // to get a more user-friendly event object
+      const deployedEvent = contract.interface.parseLog(log)
+
+      // this will contain the args object with all the event arguments
+      setCreatedRefunder(deployedEvent.args?.refunderAddress)
+    }
+
+    if (contract && state.receipt) {
+      getConfirmedEvents()
+    }
+  }, [state, contract])
+
   if (state.status == 'Mining') {
     return (
       <>
         <Button isLoading loadingText="tx Pending" />
+      </>
+    )
+  }
+
+  if (state.status == 'Success' && createdRefunder) {
+    return (
+      <>
+        <Button
+          colorScheme="teal"
+          onClick={() =>
+            navigate(
+              routes.refundables({
+                refunder: createdRefunder.toLocaleLowerCase(),
+              })
+            )
+          }
+        >
+          Manage Refunder
+        </Button>
       </>
     )
   }
